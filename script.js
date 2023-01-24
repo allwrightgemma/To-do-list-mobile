1853c0b68bed1c9ccf4a7763d584a91eca8134ed
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2023-01-24 20:52:52 +0000

    Created new list item elements and added icon

--- script.js
+++ script.js
@@ -3,16 +3,24 @@
 // Add styling
 // Delete from list - select index of the item and remove it - splice
 
-let list = [];
 const itemInput = document.querySelector('#add-item');
+const list = document.querySelector('.list');
 
-function newItem() {
-	const newItem = itemInput.value;
-	console.log(newItem);
+function addNewItem() {
+	const newItem = document.createElement('div');
+	const newCheckbox = document.createElement('input');
+	newCheckbox.setAttribute('type', 'checkbox');
+	const newText = document.createElement('p');
+	newText.innerText = itemInput.value;
+	newItem.appendChild(newCheckbox);
+	newItem.appendChild(newText);
+	list.appendChild(newItem);
+	newItem.classList.add('item');
+	itemInput.value = '';
 }
 
 itemInput.addEventListener('keypress', (e) => {
 	if (e.key === 'Enter') {
-		newItem();
+		addNewItem();
 	}
 });
