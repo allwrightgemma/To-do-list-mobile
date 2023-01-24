d3b62db4e1eb5d5d80d7609b4f1cc1f06005cb21
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2023-01-24 17:45:58 +0000

    Created function for new items

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert('Hello');
+// Empty array and push each item into it
+// Collect data from input and push into array
+// Add styling
+// Delete from list - select index of the item and remove it - splice
+
+let list = [];
+const itemInput = document.querySelector('#add-item');
+
+function newItem() {
+	const newItem = itemInput.value;
+	console.log(newItem);
+}
+
+itemInput.addEventListener('keypress', (e) => {
+	if (e.key === 'Enter') {
+		newItem();
+	}
+});
